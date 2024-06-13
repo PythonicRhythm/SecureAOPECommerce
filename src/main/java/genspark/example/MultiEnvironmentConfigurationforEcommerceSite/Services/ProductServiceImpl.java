99131e320f546c358d0c4bc6ed83f8fe4c8417ab
pokeman450 <tman45048@gmail.com>
2024-06-13 14:35:15 -0400

    Service Push
    Service implemented

--- src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Services/ProductServiceImpl.java
+++ src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Services/ProductServiceImpl.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.yaml.snakeyaml.nodes.Tag.STR;
 
 @Service
 public class ProductServiceImpl implements ProductService{
@@ -20,6 +23,7 @@ public class ProductServiceImpl implements ProductService{
     public List<Product> getAllProducts() {
         return this.productDao.findAll();
     }
+<<<<<<< Updated upstream
 
     @Override
     public List<Product> getBySellers(String seller) {
@@ -77,7 +81,107 @@ public class ProductServiceImpl implements ProductService{
     }
 
     public Product getProductById(int id) {
+=======
+>>>>>>> Stashed changes
 
+    @Override
+    public List<Product> getBySellers(String seller) {
+        logger.info(STR."Attempting to retreive Products by Seller: \{seller}");
+        return this.productDao.findBySeller(seller);
     }
+
+    public Product getById(long productID) {
+        logger.info(STR."Attempting to Retrieve product by ID: \{productID}");
+        Optional<Product> t = this.productDAO.findById(productID);
+        Product product = null;
+        if (t.isPresent()) {
+            product = t.get();
+            logger.info(STR."Successfully Retrieved product with ID: \{productID}");
+        }else {
+            logger.info(STR."Product Not Found by ID: \{productID}");
+        }
+        return product;
+    }
+
+    @Override
+    public List<Product> getByName(String name) {
+        //logger.info(STR."Attempting to Retreieve Products by name: \{name}");
+        return this.productDao.findByName(name);
+    }
+
+    @Override
+    public List<String> getByNames() {
+        logger.info("Attempting to retrieve all products by Name:");
+        List<Product> allProducts = this.productDao.findAll();
+        List<String> productNames = new ArrayList<>() ;
+        for( Product product: allProducts){
+            productNames.add(product.getName());
+        }
+        return productNames;
+    }
+
+    @Override
+    public List<String> getBySellers() {
+        return this.productDao.findBySellers();
+    }
+
+    @Override
+    public List<Product> getByCategory(String category) {
+        List<Product> allProducts = this.productDao.findAll();
+        List<Product> matchCategory = new ArrayList<Product>;
+        for( Product product: allProducts){
+            for(String productCategory : product.getCategories()){
+                if(productCategory.toLowerCase().contains(category.toLowerCase())){
+                    matchCategory.add(product);
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public List<Product> getBySorted() {
+        logger.info("Attempting to retrieve sorted products");
+
+        return this.productDao.findProductsByNameSort();
+    }
+
+    @Override
+    public List<Product> addMutlipleProduct(List<Product> products, int amount) {
+        List<Product> newProducts = new ArrayList<>();
+        for(Product product: this.getAllProducts()){
+            for(int i =0; i<amount; i++){
+                newProducts.add(product);
+            }
+        }
+        return this.productDao.saveAll(newProducts);
+    }
+
+    @Override
+    public List<Product> addProduct(List<Product> products) {
+        if(products.size() == 1){
+            return List.of(this.productDao.save(products.getFirst()));
+        }
+        else{
+            return this.productDao.saveAll(products);
+        }
+    }
+
+    @Override
+    public List<Product> updateProduct(List<Product> products) {
+        if(products.size() == 1){
+            return List.of(this.productDao.save(products.getFirst()));
+        }
+        else{
+            return this.productDao.saveAll(products);
+        }
+    }
+
+    @Override
+    public String deleteProduct(long productID) {
+        this.productDao.deleteByID(productID);
+        return "Product Deleted Successfully";
+    }
+
 
 }
