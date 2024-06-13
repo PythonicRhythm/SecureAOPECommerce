738aa520e273f43b95ca1b015c01b9a31f963112
Hy <hyhuynh222@gmail.com>
2024-06-13 07:35:38 -0700

    1st Commit

---
+++ src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/MultiEnvironmentConfigurationforEcommerceSiteApplication.java
@@ -0,0 +1,13 @@
+package genspark.example.MultiEnvironmentConfigurationforEcommerceSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiEnvironmentConfigurationforEcommerceSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiEnvironmentConfigurationforEcommerceSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/MultiEnvironmentConfigurationforEcommerceSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package genspark.example.MultiEnvironmentConfigurationforEcommerceSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiEnvironmentConfigurationforEcommerceSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
