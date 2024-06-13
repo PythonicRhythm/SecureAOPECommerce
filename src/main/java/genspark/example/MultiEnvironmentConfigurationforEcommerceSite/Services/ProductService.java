59deba1962de7570ff5605210c709f7faff869e7
pokeman450 <53342580+pokeman450@users.noreply.github.com>
2024-06-13 14:37:50 -0400

    Update ProductService.java

--- src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Services/ProductService.java
+++ src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Services/ProductService.java
@@ -5,25 +5,14 @@ import java.util.List;
 public interface ProductService {
     List<Product> getAllProducts();
     List<Product> getBySellers(String seller);
-<<<<<<< Updated upstream
-    List<Product> getById(long id);
-=======
     Product getById(long productID);
->>>>>>> Stashed changes
     List<Product> getByName(String name);
     List<String> getByNames();
     List<String> getBySellers();
     List<Product> getByCategory(String category);
     List<Product> getBySorted();
-<<<<<<< Updated upstream
-    List<Product> addMultipleProduct(List<Product> products, int amount);
-    List<Product> addProduct();
-    List<Product> updateProduct();
-    String deleteProduct();
-=======
     List<Product> addMutlipleProduct(List<Product> products, int amount);
     List<Product> addProduct(List<Product> products);
     List<Product> updateProduct(List<Product> products);
     String deleteProduct(long productID);
->>>>>>> Stashed changes
 }
