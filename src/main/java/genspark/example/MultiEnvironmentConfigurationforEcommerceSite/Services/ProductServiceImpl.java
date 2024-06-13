b3d033cc4480d5a1b2fd46bc8bfeab55a4a376a4
pokeman450 <53342580+pokeman450@users.noreply.github.com>
2024-06-13 15:36:53 -0400

    logging info added

--- src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Services/ProductServiceImpl.java
+++ src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Services/ProductServiceImpl.java
@@ -12,40 +12,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.yaml.snakeyaml.nodes.Tag.STR;
 
 @Service
 public class ProductServiceImpl implements ProductService{
     public Logger logger = LoggerFactory.getLogger(ProductController.class);
     @Autowired
-    public ProductDao productDao;
+    public ProductDAO productDao;
 
     public List<Product> getAllProducts() {
-        return this.productDao.findall();
+        logger.info("Attempting to retrieve all products");
+        return this.productDao.findAll();
     }
 
     @Override
     public List<Product> getBySellers(String seller) {
-        logger.info(STR."Attempting to retreive Products by Seller: \{seller}");
+        logger.info(String.format("Attempting to retrieve Products by Seller: %s", seller));
         return this.productDao.findBySeller(seller);
     }
 
     public Product getById(long productID) {
-        logger.info(STR."Attempting to Retrieve product by ID: \{productID}");
-        Optional<Product> t = this.productDAO.findById(productID);
+
+        logger.info(String.format("Attempting to Retrieve product by ID: %d", productID));
+        Optional<Product> t = this.productDao.findById(productID);
         Product product = null;
         if (t.isPresent()) {
             product = t.get();
-            logger.info(STR."Successfully Retrieved product with ID: \{productID}");
+            logger.info(String.format("Successfully Retrieved product with ID: %d", productID));
         }else {
-            logger.info(STR."Product Not Found by ID: \{productID}");
+            logger.info(String.format("Product Not Found by ID: %d", productID));
+
         }
         return product;
     }
 
     @Override
     public List<Product> getByName(String name) {
-        //logger.info(STR."Attempting to Retreieve Products by name: \{name}");
+        logger.info(String.format("Attempting to Retrieve Products by name: %s", name));
         return this.productDao.findByName(name);
     }
 
@@ -62,13 +64,16 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public List<String> getBySellers() {
+        logger.info("Attempting to retrieve all products by Seller:");
         return this.productDao.findBySellers();
     }
 
     @Override
     public List<Product> getByCategory(String category) {
+        logger.info(String.format("Attempting to Retrieve Products by category: %s", category));
         List<Product> allProducts = this.productDao.findAll();
-        List<Product> matchCategory = new ArrayList<Product>;
+        List<Product> matchCategory = new ArrayList<>();
+
         for( Product product: allProducts){
             for(String productCategory : product.getCategories()){
                 if(productCategory.toLowerCase().contains(category.toLowerCase())){
@@ -77,6 +82,7 @@ public class ProductServiceImpl implements ProductService{
                 }
             }
         }
+        return matchCategory;
     }
 
     @Override
@@ -88,8 +94,9 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public List<Product> addMutlipleProduct(List<Product> products, int amount) {
+        logger.info(String.format("Attempting to add Products: %s times %d", products,amount));
         List<Product> newProducts = new ArrayList<>();
-        for(Product product: this.getAllProducts()){
+        for(Product product: product()){
             for(int i =0; i<amount; i++){
                 newProducts.add(product);
             }
@@ -100,9 +107,11 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public List<Product> addProduct(List<Product> products) {
         if(products.size() == 1){
-            return List.of(this.productDao.save(products.getFirst()));
+            logger.info("adding a single product");
+            return List.of(this.productDao.save(products.get(0)));
         }
         else{
+            logger.info("adding a multiple products");
             return this.productDao.saveAll(products);
         }
     }
@@ -110,16 +119,19 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public List<Product> updateProduct(List<Product> products) {
         if(products.size() == 1){
-            return List.of(this.productDao.save(products.getFirst()));
+            logger.info("updating a single product");
+            return List.of(this.productDao.save(products.get(0)));
         }
         else{
+            logger.info("updating a multiple products");
             return this.productDao.saveAll(products);
         }
     }
 
     @Override
     public String deleteProduct(long productID) {
-        this.productDao.deleteByID(productID);
+        logger.info(String.format("Attempting to delete Product by product ID: %s", productID));
+        this.productDao.deleteById(productID);
         return "Product Deleted Successfully";
     }
 
