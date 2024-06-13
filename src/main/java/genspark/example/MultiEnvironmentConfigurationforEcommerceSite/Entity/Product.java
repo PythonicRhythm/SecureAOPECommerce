90f5b7ff4d916b0638c0421a409c5e181ed67932
Anthony Alvarez <101856161+PythonicRhythm@users.noreply.github.com>
2024-06-13 10:32:20 -0500

    Product Entity

---
+++ src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Entity/Product.java
@@ -0,0 +1,49 @@
+package genspark.example.MultiEnvironmentConfigurationforEcommerceSite.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long productid;
+    private String name;
+    private String description;
+    private double price;
+    private String seller;
+    private String[] categories;
+
+    public long getProductid() {
+        return productid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public String[] getCategories() {
+        return categories;
+    }
+}
