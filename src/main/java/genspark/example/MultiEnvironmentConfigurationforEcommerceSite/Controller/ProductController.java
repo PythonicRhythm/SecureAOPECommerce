6300bc8fc2f50b8303e942d49ef6a1bfd96356b0
Anthony Alvarez <101856161+PythonicRhythm@users.noreply.github.com>
2024-06-14 16:20:12 -0500

    Slight adjustments

--- src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Controller/ProductController.java
+++ src/main/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/Controller/ProductController.java
@@ -7,6 +7,7 @@ import genspark.example.MultiEnvironmentConfigurationforEcommerceSite.Validation
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -33,7 +34,6 @@ public class ProductController {
     public String getProducts(Model model){
         List<Product> listOfProducts =  this.ps.getAllProducts();
         model.addAttribute("productlist", listOfProducts);
-        System.out.println(listOfProducts);
         return "products";
     }
 
