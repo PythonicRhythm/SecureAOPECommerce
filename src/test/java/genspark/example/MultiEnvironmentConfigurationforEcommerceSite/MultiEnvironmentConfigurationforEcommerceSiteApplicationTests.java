54f06e0b155d2e4b9373d7cef82e024d151ce26b
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-17 07:27:27 -0700

    added a simple test

--- src/test/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/MultiEnvironmentConfigurationforEcommerceSiteApplicationTests.java
+++ src/test/java/genspark/example/MultiEnvironmentConfigurationforEcommerceSite/MultiEnvironmentConfigurationforEcommerceSiteApplicationTests.java
@@ -1,27 +1,38 @@
 package genspark.example.MultiEnvironmentConfigurationforEcommerceSite;
 
 import genspark.example.MultiEnvironmentConfigurationforEcommerceSite.Entity.Product;
-import genspark.example.MultiEnvironmentConfigurationforEcommerceSite.Repository.ProductDAO;
 import genspark.example.MultiEnvironmentConfigurationforEcommerceSite.Services.ProductServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 @SpringBootTest
 @ActiveProfiles("test")
 class MultiEnvironmentConfigurationforEcommerceSiteApplicationTests {
 
-	@Test
-	void testAddProduct() {
-	}
+    @Autowired
+    ProductServiceImpl service;
+
+    @Test
+    void contextLoads() {
+        assertThat(service).isNotNull();
+    }
+
+    @Test
+    void controllerCRUDTest() {
+        String expectedName = "Test";
+        String expectedDescription = "This is a test";
+        double expectedPrice = 9.99;
+        String expectedSeller = "Someone";
+        String[] expectedCategories = {"test", "category"};
+        Product expectedProduct = new Product(0, expectedName, expectedDescription, expectedPrice, expectedSeller, expectedCategories);
+
+        Product actualProduct = service.addProduct(expectedProduct);
+        assertEquals(expectedName, actualProduct.getName());
+    }
 
 }
